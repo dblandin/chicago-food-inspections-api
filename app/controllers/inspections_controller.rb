eda08ccf9bec5696e9d9ee8eda239bfcc23d77d0
Devon Blandin <dblandin@gmail.com>
2013-03-18 22:32:12 -0500

    Add search class. Allow search by establishment name.

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,9 +1,28 @@
 class InspectionsController < ApplicationController
   def index
-    render json: Inspection.all
+    if query = params[:name]
+      inspections = Search.new(:aka_name, query).results
+    else
+      inspections = Inspection.all
+    end
+
+    render json: inspections
   end
 
   def show
-    render json: Inspection.where(inspection_id: params[:id]).first
+    render json: Search.new(params[:id]).results.first
+  end
+
+  class Search
+    attr_reader :column, :query
+
+    def initialize(column = :inspection_id, query)
+      @column = column
+      @query = query
+    end
+
+    def results
+      @_results ||= Inspection.where("lower(#{column}) like ?", "%#{query.downcase}%")
+    end
   end
 end
